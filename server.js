8854ac27c38ebff59f9f521f1cb7c2b31c993a55
Sean Gallagher <seangllghr@gmail.com>
2020-06-07 14:26:20 -0400

    add variable for server URL

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const serverUrl = `http://localhost:${port}`
 
 app.get('/', (req, res) => res.send('Hello, World!'))
 
-app.listen(port, () => console.log(`Hello World listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`Hello World listening on ${serverUrl}`))
