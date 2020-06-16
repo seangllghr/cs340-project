ffc133a99a38256ae9cce39dabc477554de71828
Sean Gallagher <seangllghr@gmail.com>
2020-06-16 17:44:32 -0400

    created updateService and deleteService tests

---
+++ tests/02-api/23-updateService.test.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const assert = require('assert')
+const services = require('../../services')
+const { dataRead } = require('../../db')
+const { testMessage } = require('../testMessage')
+const { id: targetId } = require('./create.json')
+
+;()()
