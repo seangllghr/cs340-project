61f8905ceaff3623bf54e20d06283dfa55ea832e
Sean Gallagher <seangllghr@gmail.com>
2020-06-16 00:52:09 -0400

    added the controllers file

---
+++ controllers/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const services = require('../services')
+
+async function createController (req, res, next) {
+  // TODO: Create logic
+}
+
+async function readController (req, res, next) {
+  // TODO: Read logic
+}
+
+async function updateController (req, res, next) {
+  // TODO: Update logic
+}
+
+async function deleteController (req, res, next) {
+  // TODO: Delete logic
+}
+
+module.exports = {
+  createController: createController,
+  readController: readController,
+  updateController: updateController,
+  deleteController: deleteController
+}
