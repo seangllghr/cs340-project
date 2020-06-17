24a426628c80c16ed708574197b1e552ca252774
Sean Gallagher <seangllghr@gmail.com>
2020-06-16 23:03:34 -0400

    wrote test logic for updateService

--- tests/02-api/23-updateService.test.js
+++ tests/02-api/23-updateService.test.js
@@ -6,4 +6,25 @@ const { dataRead } = require('../../db')
 const { testMessage } = require('../testMessage')
 const { id: targetId } = require('./create.json')
 
-;()()
+;(async (testName, query, update) => {
+  try {
+    await services.updateService(query, update)
+    const results = await dataRead(query)
+    results.forEach((result) => {
+      assert.strictEqual(result.result, 'Violation Issued')
+    })
+    testMessage(testName, true)
+  } catch (err) {
+    const { expected, actual } = err
+    if (err.constructor === assert.AssertionError) {
+      testMessage(testName, false)
+      console.log(`Expected: ${expected} Actual: ${actual}`)
+    } else {
+      console.log(err)
+    }
+  }
+})(
+  'updateService: Updated record shows updated field value',
+  { id: targetId },
+  { $set: { result: 'Violation Issued' } }
+)
