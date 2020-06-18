0a7f0d9ca11147ff600e068e831299d42e89cdbd
Sean Gallagher <seangllghr@gmail.com>
2020-06-18 15:01:01 -0400

    added labels to curls.sh so results are clearer

--- tests/02-api/curls.sh
+++ tests/02-api/curls.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Testing Create API Endpoint:"
 curl \
     -H "Content-Type: application/json" \
     -X POST \
@@ -7,17 +8,22 @@ curl \
     'http://localhost:3000/api/create'
 echo ""
 
+echo "Testing Read API Endpoint:"
 curl 'http://localhost:3000/api/read?business_name=ACME+TEST+INC.'
 echo ""
 
+echo "Testing Update API Endpoint:"
 curl 'http://localhost:3000/api/update?id=10011-2017-TEST&result=Violation+Issued'
 echo ""
 
+echo "Verification:"
 curl 'http://localhost:3000/api/read?business_name=ACME+TEST+INC.'
 echo ""
 
+echo "Testing Delete API Endpoint:"
 curl 'http://localhost:3000/api/delete?id=10011-2017-TEST'
 echo ""
 
+echo "Verification:"
 curl 'http://localhost:3000/api/read?business_name=ACME+TEST+INC.'
 echo ""
