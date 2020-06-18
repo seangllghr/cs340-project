9d63257c802ccb841ab0350889889f18c9ece2ba
Sean Gallagher <seangllghr@gmail.com>
2020-06-17 21:21:52 -0400

    tidied up output for curl script

--- tests/02-api/curls.sh
+++ tests/02-api/curls.sh
@@ -11,9 +11,5 @@ curl 'http://localhost:3000/api/read?business_name=ACME+TEST+INC.'
 echo ""
 curl 'http://localhost:3000/api/update?id=10011-2017-TEST&result=Violation+Issued'
 echo ""
-curl 'http://localhost:3000/api/update?id=10011-2017'
-echo ""
-curl 'http://localhost:3000/api/update?id=10011-2017-TEST&result=Violation+Issued&test=bad'
-echo ""
 curl 'http://localhost:3000/api/delete?id=10011-2017-TEST'
 echo ""
