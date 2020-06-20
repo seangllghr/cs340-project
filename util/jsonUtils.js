50cfb59691517483a4041cfde9b4278e5ed344b1
Sean Gallagher <seangllghr@gmail.com>
2020-06-20 15:37:51 -0400

    added logic to parse out breaking dates and oids

--- util/jsonUtils.js
+++ util/jsonUtils.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const mongo = require('mongodb')
+
 /**
  * Reads a readable stream and transforms it into a string
  * @param {Object} stream - a readable stream
@@ -15,16 +17,28 @@ function streamToString (stream) {
 }
 
 /**
- * Parses a JSON stream into a native object
+ * Parses a JSON stream into a native object representation of a MongoDB doc
  * @param {Object} stream - a readable stream
  * @returns {Object} - the object parsed from the stream
  */
-async function jsonStreamToObj (stream) {
+async function jsonStreamToMongoDoc (stream) {
   const jsonString = await streamToString(stream)
-  return JSON.parse(jsonString)
+  const mongoObj = JSON.parse(jsonString)
+  for (const field in mongoObj) {
+    if (typeof mongoObj[field] === 'object') {
+      for (const fieldType in mongoObj[field]) {
+        if (fieldType === '$oid') {
+          mongoObj[field] = new mongo.ObjectID(mongoObj[field][fieldType])
+        } else if (fieldType === '$date') {
+          mongoObj[field] = new Date(mongoObj[field][fieldType])
+        }
+      }
+    }
+  }
+  return mongoObj
 }
 
 module.exports = {
   streamToString: streamToString,
-  jsonStreamToObj: jsonStreamToObj
+  jsonStreamToMongoDoc: jsonStreamToMongoDoc
 }
