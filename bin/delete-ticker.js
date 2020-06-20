c0e9ac18c728620e46d7b0bd2d145a6d7805983c
Sean Gallagher <seangllghr@gmail.com>
2020-06-20 15:31:25 -0400

    wrote delete by ticker util

---
+++ bin/delete-ticker.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const parseArgs = require('minimist')
+const { getTickerString } = require('./cli-utils')
+const db = require('../db')
+
+async function deleteStockByTicker (argv) {
+  let ticker
+  try {
+    ticker = await getTickerString(argv)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  const query = { Ticker: ticker }
+
+  try {
+    const result = await db.dataDelete(query)
+    if (result.result.n === 1) {
+      console.log(`Deleted stock record for Ticker ${ticker}`)
+    } else if (result.result.n > 1) {
+      console.log(
+        `Deleted more than one record for ${ticker}. You may want to check ` +
+          'the database for inconsistencies.'
+      )
+    } else {
+      console.log('No matching record found')
+    }
+  } catch (err) {
+    console.log('Something went wrong')
+    console.log(err)
+  }
+}
+
+;(async () => {
+  const argv = parseArgs(process.argv.slice(2))
+  deleteStockByTicker(argv)
+})()
