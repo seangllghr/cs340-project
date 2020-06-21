a77b70e439dc8182ca4f0b3f3b58ef490be58efd
Sean Gallagher <seangllghr@gmail.com>
2020-06-20 20:12:21 -0400

    implemented sma-spread.js
    Turns out things really are simple when you've written the pieces
    correctly...

--- bin/sma-spread.js
+++ bin/sma-spread.js
@@ -2,8 +2,32 @@
 
 const parseArgs = require('minimist')
 const { promptForNumber } = require('./cli-utils')
+const { countMatching } = require('../db')
+
+async function findSMASpread (argv) {
+  const lo = Number(argv.l) || promptForNumber('Low:')
+  const hi = Number(argv.h) || promptForNumber('High:')
+
+  const query = {
+    '50-Day Simple Moving Average': {
+      $gt: lo,
+      $lt: hi
+    }
+  }
+  const result = await countMatching(query)
+  console.log(`Found ${result} matching records`)
+}
 
 ;(async () => {
   const argv = parseArgs(process.argv.slice(0))
-  console.log(argv)
+  if (argv.help) {
+    console.log('Usage: sma-spread.js [-l <low value>] [-h <high value>]')
+    process.exit(0)
+  }
+  try {
+    await findSMASpread(argv)
+  } catch (err) {
+    console.log('Something went wrong')
+    console.log(err)
+  }
 })()
