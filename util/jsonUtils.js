fe2fd9627959ebb02bc202eb7baa6ded92edca64
Sean Gallagher <seangllghr@gmail.com>
2020-06-22 16:16:07 -0400

    pull special field logic out, alphebetize funcs
    This looks like a big commit, but it doesn't really change much. It
    mostly reorganizes (alphebetizes) the functions. I did pull the special
    field logic out into its own function so I could use it in the create
    service.

--- util/jsonUtils.js
+++ util/jsonUtils.js
@@ -3,27 +3,17 @@
 const mongo = require('mongodb')
 
 /**
- * Reads a readable stream and transforms it into a string
- * @param {Object} stream - a readable stream
- * @returns {string} - a UTF8 string
+ * MongoDB dumps occasionally contain $oid and $date query fields, which
+ * represent native MongoDB ObjectID and JavaScript Date objects, respectively.
+ * This function parses an object imported from stringified JSON data and
+ * replaces any occurrences of those fields with the respective native objects,
+ * because not doing this apparently breaks MongoDB's Node driver in insidious
+ * ways.
+ *
+ * @param {Object} mongoObj - the MongoDB object that may contain special fields
+ * @returns {Object} - the object with these fields replaced with real objects
  */
-function streamToString (stream) {
-  const chunks = []
-  return new Promise((resolve, reject) => {
-    stream.on('data', (chunk) => chunks.push(chunk))
-    stream.on('error', reject)
-    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
-  })
-}
-
-/**
- * Parses a JSON stream into a native object representation of a MongoDB doc
- * @param {Object} stream - a readable stream
- * @returns {Object} - the object parsed from the stream
- */
-async function jsonStreamToMongoDoc (stream) {
-  const jsonString = await streamToString(stream)
-  const mongoObj = JSON.parse(jsonString)
+function convertMongoQueryFields (mongoObj) {
   for (const field in mongoObj) {
     if (typeof mongoObj[field] === 'object') {
       for (const fieldType in mongoObj[field]) {
@@ -38,7 +28,34 @@ async function jsonStreamToMongoDoc (stream) {
   return mongoObj
 }
 
+/**
+ * Parses a JSON stream into a native object representation of a MongoDB doc
+ * @param {Object} stream - a readable stream
+ * @returns {Object} - the object parsed from the stream
+ */
+async function jsonStreamToMongoDoc (stream) {
+  const jsonString = await streamToString(stream)
+  let mongoObj = JSON.parse(jsonString)
+  mongoObj = convertMongoQueryFields(mongoObj)
+  return mongoObj
+}
+
+/**
+ * Reads a readable stream and transforms it into a string
+ * @param {Object} stream - a readable stream
+ * @returns {string} - a UTF8 string
+ */
+function streamToString (stream) {
+  const chunks = []
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(chunk))
+    stream.on('error', reject)
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+  })
+}
+
 module.exports = {
-  streamToString: streamToString,
-  jsonStreamToMongoDoc: jsonStreamToMongoDoc
+  convertMongoQueryFields: convertMongoQueryFields,
+  jsonStreamToMongoDoc: jsonStreamToMongoDoc,
+  streamToString: streamToString
 }
